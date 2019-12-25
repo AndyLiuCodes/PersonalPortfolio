c51278e73ef0fe392f28f2a299ca3875adfa6296
Andy Liu <andyliu5898@gmail.com>
2019-12-25 03:14:08 -0800

    added bootstrap and jquery

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5000;
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
