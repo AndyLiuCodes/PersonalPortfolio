ff00402e6cb36abfc08442b26201f06c7b942818
Andy Liu <andyliu5898@gmail.com>
2019-12-29 02:27:42 -0800

    created backend

---
+++ server/routes/api/posts.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+//Get Posts
+router.get('/', async (req, res) =>{
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+}); 
+// it actually references /api/posts/
+
+//Add Posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    
+    //this is the data that should be coming from vue.js
+    await posts.insertOne({
+       text: req.body.text,
+       createdAt: new Date() 
+    });
+
+    res.status(201).send();
+});
+
+//Delete Posts
+router.delete('/:id', async (req, res) =>{
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://root123:1927Brownst@personalwebsite-jnyn0.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+
+    return client.db('PersonalWebsite').collection('posts');
+}
+
+module.exports = router;
